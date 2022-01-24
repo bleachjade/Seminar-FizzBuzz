e7e7fc4e86b39a385617e0baac59790ac65cdac0
Nattapol Boonyapornpong <46812243+bleachjade@users.noreply.github.com>
2022-01-24 15:52:49 +0700

    initial file commit

---
+++ fizz_buzz.py
@@ -0,0 +1,14 @@
+class FizzBuzzRefactored:
+
+    def fizzbuzz(i):
+        if i % 15 == 0:
+            return "FizzBuzz"
+        elif i % 3 == 0:
+            return "Fizz"
+        elif i % 5 == 0:
+            return "Buzz"
+        else:
+            return i
+
+
+FizzBuzz = FizzBuzzRefactored
